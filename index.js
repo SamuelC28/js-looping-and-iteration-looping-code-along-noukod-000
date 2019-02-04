6f092ed5c317ff83c4a4a3525943ddbf749e146d
Samuel Cornet <samuel@noukod.com>
2019-02-04 15:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+// let spy = ['Lisa', 'Kaitlin', 'Jan'];
+
+
+function printBadges(spy) {
+  for (let i = 0; i < spy.length; i++) {
+    console.log(`Welcome ${spy[i]}! You are employee #${i+1}.`);
+  }
+  return spy;
+}
+
+
+function tailsNeverFails(){
+  let tails = 0;
+  while tails (Math.random() >=0.4){
+    tails++;
+  }
+  return `You got ${Math.random()} tails in a row!`)
+}
